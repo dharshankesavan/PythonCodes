b978d5df9b9582667f267a63c38093ab1b16264f
DHARSHAN KESAVAN <dharshankesavanpotty@gmail.com>
2019-11-11 15:13:37 +0000

    Removed unnecessary code

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
 # My Python Codes
-#Below Function will add two list and return new list
-def add_list(list_a, list_b):
-    print("Add List");
-    return list_a + list_b
-
-list_A = [1,2,3]
-list_B = [4,5,6]
-print(add_list(list_A, list_B));
