5870f25350085c7003ba06abbaac030c28eb61ed
DHARSHAN KESAVAN <38592943+dharshankesavan@users.noreply.github.com>
2019-11-13 00:25:02 +0530

    Add files via upload
    Solution for the Assignment

--- Good String.py
+++ Good String.py
@@ -5,3 +5,32 @@ Created on Tue Nov 12 23:53:32 2019
 @author: DHARSHAN
 """
 
+def CreateWord (MainString):
+    possibleWords = []
+    for i in range(len(MainString)):
+        for j in range(i+1,len(MainString)):
+            possibleWords.append(MainString[i:j+1])
+    return (possibleWords);
+
+def CheckRepeat (CheckString):
+    count = 0
+    for x in range(0, len(CheckString)):
+        for y in range(x+1, len(CheckString)):
+            if CheckString[x] == CheckString[y]:
+                count = count + 1
+    return count;
+                
+    
+
+MainString = "abcdeefghij"
+WordsList = CreateWord(MainString)
+maxValue = 0
+GoodString = ""
+for eachWord in WordsList:
+    if (CheckRepeat(eachWord) == 0):
+        if (len (eachWord) > maxValue):
+            maxValue = len(eachWord)
+            GoodString = eachWord
+print ("Good String is:-%s and it's length is:-%s" %(GoodString,maxValue));
+        
+        
